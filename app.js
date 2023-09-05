f46c939ba6c84f914c1efaa6c2c6400b19c63645
Jibril <jibrilabdi01@gmail.com>
2023-09-05 22:44:51 +0100

    add final script code

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
-console.log("hello")
+function getComputerChoice () {
+    let computerChoice = ["r", "p", "s"]
+    let randomChoice = Math.floor(Math.random() * computerChoice.length + 1)
+    
+    if (randomChoice === 1) {
+        return "rock"
+    } else if (randomChoice === 2) {
+        return "paper" 
+    } else 
+        return "scissors"
+}
+
+function getPlayerChoice () {
+    let playerChoice = prompt("What do you draw?: Enter 'rock', 'paper, or 'scissors'")
+    let response = playerChoice.toLowerCase()
+    return response
+    }
+
+
+function playRound () {
+    let playerSelection = getPlayerChoice()
+    let computerSelection = getComputerChoice()
+    console.log(`You played '${playerSelection}' and the computer played '${computerSelection}'`)
+
+
+    if (playerSelection == computerSelection) {
+        return "It's a tie!" 
+    }
+    else if (playerSelection == "rock" && computerSelection == "paper" || playerSelection == "paper" && computerSelection == "scissors" || playerSelection == "scissors" && computerSelection == "rock") {
+        return "Computer Wins!"
+    }
+    else if (playerSelection == "rock" && computerSelection == "scissors" || playerSelection == "paper" && computerSelection == "rock" || playerSelection == "scissors" && computerSelection == "paper") {
+        return "You Win!"
+    }
+    else {
+        return "Please enter a correct value"
+    }
+}
+
+console.log(playRound())
